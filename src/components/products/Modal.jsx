26283409dfef9f7ee3a46a48389c431a1e5833c7
Victor Blanco <12vblanco@gmail.com>
2024-06-17 18:43:29 +0100

    rollback

--- src/components/products/Modal.jsx
+++ src/components/products/Modal.jsx
@@ -11,7 +11,7 @@ const Modal = ({ handleClose }) => {
   const cart = useContext(CartContext);
 
   const checkout = async () => {
-    await fetch("http://localhost:4000/checkout", {
+    await fetch("/netlify/functions/checkout.js", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
