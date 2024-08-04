806abd223518e17afebe6039bc6dd26e319ec1d3
Victor Blanco <12vblanco@gmail.com>
2024-08-04 10:09:25 +0100

    form fix

--- src/components/contact/Success.jsx
+++ src/components/contact/Success.jsx
@@ -27,33 +27,11 @@ const Div = styled.div`
   flex-direction: column;
   width: 100%;
   height: 90vh;
-  border-bottom: solid 1px #000;
-  border-right: solid 1px #000;
 `;
 
 const Text = styled.p`
   margin-bottom: 44px;
   font-weight: 500;
-`;
-
-const ContactBtn = styled.button`
-  display: flex;
-  background: var(--blue);
-  border: 1px solid var(--blue);
-  border-radius: 2px;
-  font-weight: 600;
-  font-size: 0.9375rem;
-  color: var(--white);
-  width: 110px;
-  height: 50px;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  transition: all 0.3s linear;
-
-  &:hover {
-    background: rgba(40, 98, 150, 1) 100%;
-  }
 `;
 
 export default Success;
